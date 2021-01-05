479e9699577f7ed620a0ccdfac532c5fa49da44f
Izzy Spering <43628079+IzzyBoop@users.noreply.github.com>
2021-01-05 11:14:41 -0500

    Create Master.ps1

---
+++ Master.ps1
@@ -0,0 +1,13 @@
+$date = get-date -f yyyy-MM-dd
+
+.\speedtest.exe --format=json-pretty > "Results\results_$($date).json"
+
+$path = "\\VTN-FS01\4TB Shared\Programming\SpeedTest CLI"
+
+$data = Get-Content -Raw -Path "$($Path)\Results\results_$($date).json" | ConvertFrom-Json
+
+$ping = [math]::round($data.ping.latency,2)
+$downloadMbits = [math]::round(($data.download.bytes / 125000) / ($data.download.elapsed / 1000),2)
+$uploadMbits = [math]::round(($data.upload.bytes / 125000) / ($data.upload.elapsed / 1000),2)
+
+Add-Content "$($Path)\Results\Results Master.txt" "Date: $date | Download: $downloadMbits | Upload: $uploadMbits | Ping: $ping `n"
